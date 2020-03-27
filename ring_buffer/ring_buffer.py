a5177570563c94a37fd15a709065f87f9bd13831
Rasha Moumneh <rasha@fastmail.com>
2020-03-27 15:43:04 -0400

    more WIP on buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -9,18 +9,42 @@ class RingBuffer:
 
     def append(self, item):
         if self.storage.length == self.capacity:
-            self.storage.remove_from_tail()
-            self.storage.add_to_head(item)
+            self.current.value = item
+            self.current = self.current.next
         else:
-            self.storage.add_to_head(item)
+            self.storage.add_to_tail(item)
+            self.current = self.storage.head
 
     def get(self):
         # Note:  This is the only [] allowed
         list_buffer_contents = []
 
-        # TODO: Your code here
-
         return list_buffer_contents
+
+
+buffer = RingBuffer(5)
+buffer.append("a")
+buffer.append("b")
+buffer.append("c")
+buffer.append("d")
+# print("len", buffer.storage.length)
+print("tail", buffer.storage.tail.value)
+print(buffer.get())
+buffer.append("e")
+# print("len", buffer.storage.length)
+print("tail", buffer.storage.tail.value)
+print(buffer.get())
+buffer.append("f")
+# print("len", buffer.storage.length)
+print("tail", buffer.storage.tail.value)
+print(buffer.get())
+buffer.append("g")
+buffer.append("h")
+buffer.append("i")
+# print("len", buffer.storage.length)
+print("tail", buffer.storage.tail.value)
+print(buffer.get())
+# ["f", "g", "h", "i", "e"]
 
 
 # ----------------Stretch Goal-------------------
